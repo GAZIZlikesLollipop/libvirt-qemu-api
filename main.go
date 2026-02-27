f3494144c2887d190ff3fbddab874b2aa10756f3
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-02-27 21:07:44 +0500

    middlware features

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"libvirt.org/go/libvirt"
 )
 
@@ -26,12 +27,11 @@ type MachineState struct {
 	MaxRam    uint64 `json:"max_ram"`
 	Ram       uint64 `json:"ram"`
 	CoreCount uint   `json:"core_count"`
-	CpuTime   uint64 `json:"cup_time"`
+	CpuTime   uint64 `json:"cpu_time"`
 }
 
 func main() {
-	// cnn, err := libvirt.NewConnect("qemu:///system")
-	cnn, err := libvirt.NewConnect("qemu:///session")
+	cnn, err := libvirt.NewConnect("qemu:///system")
 	if err != nil {
 		fmt.Println("Ошибка подключения к гипервизору: ", err)
 		os.Exit(1)
@@ -44,8 +44,8 @@ func main() {
 		<vcpu>3</vcpu>
 		<os>
 			<type arch='x86_64'>hvm</type>
-			<boot dev='cdrom'/>
 			<boot dev='hd'/>
+			<boot dev='cdrom'/>
 		</os>
 		<cpu mode='host-passthrough'/>
 		<devices>
@@ -85,6 +85,11 @@ func main() {
 	}
 	defer dom.Free()
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"}, // или конкретный "*"
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+	}))
 	r.Get("/start", func(resp http.ResponseWriter, req *http.Request) {
 		if err := dom.Create(); err != nil {
 			log.Println("Ошибка запуска виртуальной машины: ", err)
@@ -93,16 +98,16 @@ func main() {
 		}
 		respondJSON(resp, 200, "Виртуальная машина успешно запущенна!")
 	})
-	r.Get("/stop", func(resp http.ResponseWriter, req *http.Request) {
-		if err := dom.Shutdown(); err != nil {
+	r.Get("/forcestop", func(resp http.ResponseWriter, req *http.Request) {
+		if err := dom.Destroy(); err != nil {
 			log.Println("Ошибка отключения виртуальной машины: ", err)
 			respondJSON(resp, 500, "Ошибка отключения виртульной машины")
 			return
 		}
 		respondJSON(resp, 200, "Виртуальная машина успешно отключена!")
 	})
-	r.Get("/forcestop", func(resp http.ResponseWriter, req *http.Request) {
-		if err := dom.Destroy(); err != nil {
+	r.Get("/stop", func(resp http.ResponseWriter, req *http.Request) {
+		if err := dom.Shutdown(); err != nil {
 			log.Println("Ошибка отключения виртуальной машины: ", err)
 			respondJSON(resp, 500, "Ошибка отключения виртульной машины")
 			return
